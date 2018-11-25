69fa36d9d41ba0427fba9cffe6c3834814d59e05
fxanomaly@gmail.com <fxanomaly@gmail.com>
2018-11-25 12:28:11 +0000

    CommentBox and CommentList render test -OK

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import {shallow} from 'enzyme'
-import App from './App';
+import App from 'App';
+import CommentBox from 'components/CommentBox'
+import CommentList from 'components/CommentList'
+
+let component;
+
+beforeEach(()=>{
+  component = shallow(<App />)
+})
 
 it('renders without crashing', () => {
-
-  shallow(<App />);
-
+  shallow(<App />)
 });
+
+it('shows a CommentBox', ()=>{
+  expect(component.find(CommentBox).length).toEqual(1)
+})
+
+it('shows a CommentList', ()=>{
+  expect(component.find(CommentList).length).toEqual(1)
+})
